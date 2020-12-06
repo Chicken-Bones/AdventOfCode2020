f5fbcbdfc0387193c55fec4ac4b3606f4e0937b0
Chicken-Bones <mehvids@gmail.com>
2020-12-06 15:40:25 +1000

    Bit of cleanup improvement (mostly for day4)

--- day4.py
+++ day4.py
@@ -1,40 +1,22 @@
 import re
 
-fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+eye_colors = 'amb blu brn gry grn hzl oth'.split(' ')
 
-
-def valid(s):
-    return sum(f in fields for f, _ in re.findall(r"(\w+):(\S+)", s)) == len(fields)
-
-
-def valid2(s):
-    return sum(valid_value(f, v) for f, v in re.findall(r"(\w+):(\S+)", s)) == len(fields)
-
-
-def valid_value(f, v):
-    if f == 'byr':
-        return 1920 <= int(v) <= 2002
-    if f == 'iyr':
-        return 2010 <= int(v) <= 2020
-    if f == 'eyr':
-        return 2020 <= int(v) <= 2030
-    if f == 'hgt':
-        if v[-2:] == 'cm':
-            return 150 <= int(v[:-2]) <= 193
-        if v[-2:] == 'in':
-            return 59 <= int(v[:-2]) <= 76
-    if f == 'hcl':
-        return re.match(r"#[0-9a-f]{6}$", v) is not None
-    if f == 'ecl':
-        return v in 'amb blu brn gry grn hzl oth'.split(' ')
-    if f == 'pid':
-        return len(v) == 9 and v.isnumeric()
-
-    return False
-
+validators = {
+    'byr': lambda v: 1920 <= int(v) <= 2002,
+    'iyr': lambda v: 2010 <= int(v) <= 2020,
+    'eyr': lambda v: 2020 <= int(v) <= 2030,
+    'hgt': lambda v:
+        150 <= int(v[:-2]) <= 193 if v[-2:] == 'cm' else
+        59 <= int(v[:-2]) <= 76 if v[-2:] == 'in' else
+        False,
+    'hcl': lambda v: re.match(r"#[0-9a-f]{6}$", v) is not None,
+    'ecl': lambda v: v in eye_colors,
+    'pid': lambda v: len(v) == 9 and v.isnumeric()
+}
 
 if __name__ == "__main__":
     with open("input/day4.txt") as file:
-        passports = file.read().split('\n\n')
-        print(sum(valid(p) for p in passports))
-        print(sum(valid2(p) for p in passports))
+        passports = [{k: v for k, v in re.findall(r"(\w+):(\S+)", s)} for s in file.read().split('\n\n')]
+        print(sum(all(k in p for k in validators) for p in passports))
+        print(sum(all(k in p and v(p[k]) for k, v in validators.items()) for p in passports))
--- day6.py
+++ day6.py
@@ -1,10 +1,11 @@
 def allforgroup(s):
-    group = s.strip().split('\n')
+    group = s.split('\n')
     return sum(all(q in p for p in group) for q in group[0])
+
 
 if __name__ == "__main__":
     with open("input/day6.txt") as file:
-        groups = file.read().split('\n\n')
+        groups = file.read().strip().split('\n\n')
         print(sum(len(set(s.replace('\n', ''))) for s in groups))
         print(sum(allforgroup(s) for s in groups))
 
