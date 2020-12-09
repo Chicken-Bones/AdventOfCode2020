01f64c1b0d1f81af122d9f38acd574be5958ab65
Chicken-Bones <mehvids@gmail.com>
2020-12-09 20:49:53 +1000

    Day 9 - inchworm method

--- day9.py
+++ day9.py
@@ -6,15 +6,25 @@ if __name__ == "__main__":
         w = 25
         for i in range(w, len(nums)):
             if not any(a+b == nums[i] for a, b in combinations(nums[i-w:i], 2)):
-                key = nums[i]
+                n = nums[i]
                 break
 
-        print(key)
+        print(n)
 
-        n = 32321523
         c = [0] + list(accumulate(nums))
         for i, j in combinations(range(len(nums)+1), 2):
             if c[j] - c[i] == n:
                 print(min(nums[i:j]) + max(nums[i:j]))
                 break
 
+        # a faster way
+        i, j, s = (0, 0, 0)
+        while s != n:
+            if s < n:
+                s += nums[j]
+                j += 1
+            else:
+                s -= nums[i]
+                i += 1
+
+        print(min(nums[i:j]) + max(nums[i:j]))
