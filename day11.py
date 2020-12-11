d78cafdba374ba7d989a03fbd4dd487512e722c2
Chicken-Bones <mehvids@gmail.com>
2020-12-11 22:14:55 +1000

    Day 11  which I hate less, but takes 10s to run

--- day11.py
+++ day11.py
@@ -1,74 +1,39 @@
-from functools import *
-import re
-from itertools import *
-
 dirs = [(1, 0), (1, 1), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
 
 
+def simulate(seats, neighbour_func, occ_threshold):
+    def occupied(x, y):
+        return sum(seats[pt] == '#' for pt in neighbour_func(x, y) if pt in seats)
+
+    def ca(v, occ):
+        return \
+            'L' if v == '#' and occ >= occ_threshold else \
+            '#' if v == 'L' and occ == 0 else \
+            v
+
+    while True:
+        new_seats = {pt: ca(v, occupied(*pt)) for pt, v in seats.items()}
+        if seats == new_seats:
+            break
+        seats = new_seats
+
+    return sum(v == '#' for v in seats.values())
 
 
 if __name__ == "__main__":
     with open("input/day11.txt") as file:
-        seats = [['.'] + list(s) + ['.'] for s in file]
-        seats = [['.'] * len(seats[0])] + seats + [['.'] * len(seats[0])]
-        w = len(seats[0])-2
-        h = len(seats)-2
+        plane = {(x, y): c for y, line in enumerate(file) for x, c in enumerate(line.strip())}
+        seats = {k: v for k, v in plane.items() if v == 'L'}
 
-        def s(): return "".join("".join(s) for s in seats)
+        def neighbours_1(x, y):
+            return ((x + dx, y + dy) for dx, dy in dirs)
+        print(simulate(seats, neighbours_1, 4))
 
-        prev = s()
-        while True:
-            seats2 = [l.copy() for l in seats]
-            for i in range(1, w+1):
-                for j in range(1, h+1):
-                    occ = [a for row in seats[j-1:j+2] for a in row[i-1:i+2]].count('#')
-                    me = seats[j][i]
-                    if me == '#' and occ >= 5:
-                        seats2[j][i] = 'L'
-                    elif me == 'L' and occ == 0:
-                        seats2[j][i] = '#'
+        def neighbours_2(x, y):
+            for dx, dy in dirs:
+                pt = (x+dx, y+dy)
+                while pt not in seats and pt in plane:
+                    pt = (pt[0]+dx, pt[1]+dy)
+                yield pt
 
-            seats = seats2
-            next = s()
-            if next == prev:
-                break
-            prev = next
-
-        print(list(prev).count('#'))
-
-
-    with open("input/day11.txt") as file:
-        seats = [['.'] + list(s.strip()) + ['.'] for s in file]
-        seats = [['.'] * len(seats[0])] + seats + [['.'] * len(seats[0])]
-        w = len(seats[0])-2
-        h = len(seats)-2
-
-        def s(): return "".join("".join(s) for s in seats)
-
-        def sight(x, y, dx, dy):
-            if x < 0 or y < 0 or x >= w+2 or y >= h+2:
-                return 0
-
-            return 1 if seats[y][x] == '#' else sight(x+dx, y+dy, dx, dy) if seats[y][x] == '.' else 0
-
-        prev = s()
-        while True:
-            seats2 = [l.copy() for l in seats]
-            for i in range(1, w+1):
-                for j in range(1, h+1):
-                    occ = 0
-                    for dx, dy in dirs:
-                        occ += sight(i+dx, j+dy, dx, dy)
-                    me = seats[j][i]
-                    if me == '#' and occ >= 5:
-                        seats2[j][i] = 'L'
-                    elif me == 'L' and occ == 0:
-                        seats2[j][i] = '#'
-
-            seats = seats2
-            next = s()
-            if next == prev:
-                break
-            prev = next
-
-        print(list(prev).count('#'))
+        print(simulate(seats, neighbours_2, 5))
