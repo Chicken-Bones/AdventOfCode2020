553036c43619f5b68e417c3aad97c69f5606b9bd
Chicken-Bones <mehvids@gmail.com>
2020-12-14 22:02:29 +1000

    Day 14

---
+++ day14.py
@@ -0,0 +1,37 @@
+if __name__ == "__main__":
+    with open("input/day14.txt") as file:
+        mask = 0
+        maskw = 0
+        mem = [0] * (1 << 16)
+        for s in file:
+            if s.startswith("mask"):
+                m = s[7:]
+                mask = int(m.replace("1", "0").replace("X", "1"), 2)
+                maskw = int(m.replace("X", "0"), 2)
+            else:
+                a, b = s.split(" = ")
+                addr = int(a[4:-1])
+                mem[addr] = (int(b) & mask) | maskw
+
+        print(sum(mem))
+
+    with open("input/day14.txt") as file:
+        mask = 0
+        maskw = 0
+        mem = dict()
+        for s in file:
+            if s.startswith("mask"):
+                m = s[7:]
+                mask = int(m.replace("1", "0").replace("X", "1"), 2)
+                maskw = int(m.replace("X", "0"), 2)
+            else:
+                a, b = s.split(" = ")
+                addr = int(a[4:-1]) & ~mask | maskw
+                masks = [mask]
+                while masks[-1] > 0:
+                    masks += [m & (masks[-1]-1) for m in masks]
+
+                for m in masks:
+                    mem[m | addr] = int(b)
+
+        print(sum(mem.values()))
